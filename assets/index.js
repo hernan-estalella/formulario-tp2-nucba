198e3cbf3f4d3feb223095a2fd0a859c7a1934bf
rocaau <ariel.aguilar@apexamerica.com>
2020-09-22 21:46:44 -0300

    javascript

---
+++ assets/index.js
@@ -0,0 +1,56 @@
+const monto = document.querySelector('#monto');
+const fecha = document.querySelector('#fecha');
+const tipo = document.querySelector('#tipo');
+const descripcion = document.querySelector('#descripcion');
+const form  = document.querySelector('#movimientos');
+const cuerpoTabla = document.querySelector('#tabla-cuerpo');
+
+let movimientos = [];
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    
+    const movimiento = {
+        
+        monto: monto.value,
+        fecha: fecha.value,
+        tipo: tipo.value,
+        descripcion: descripcion.value
+
+    }
+    movimientos = [...movimientos,movimiento]
+    render();
+    form.reset();
+    
+    console.log(movimientos);
+    
+});
+
+const render = ()=> {
+/*     movimientos.map((movimiento)=>{
+        const {monto,fecha,descripcion,tipo} = movimiento;
+
+         cuerpoTabla.innerHTML = `    <tr>
+        <td>${monto}</td>
+        <td>${descripcion}</td>
+        <td>${fecha}</td>
+        <td>${tipo}</td>
+      </tr>`
+
+    })
+    return; */
+    cuerpoTabla.innerHTML="";
+    movimientos.forEach((movimiento)=>{
+        const {monto,fecha,descripcion,tipo} = movimiento;
+
+         cuerpoTabla.innerHTML += `    <tr>
+        <td>${monto}</td>
+        <td>${descripcion}</td>
+        <td>${fecha}</td>
+        <td>${tipo}</td>
+      </tr>`
+
+    })
+    return;
+
+}
